8773d7319c269cc7da35ad935efc9fc989f225bc
giuseppecastaldo <57662742+giuseppecastaldo@users.noreply.github.com>
2022-09-05 22:53:24 +0200

    Update run.sh

--- whatsapp_addon/run.sh
+++ whatsapp_addon/run.sh
@@ -2,7 +2,7 @@
 set +u
 
 mkdir -p config/custom_components/whatsapp
-sed -i "s/{{HOSTNAME}}/$HOSTNAME/g" /custom_component/whatsapp.py
+sed -i "s/{{HOSTNAME}}/$HOSTNAME/g" custom_component/whatsapp.py
 
 cp --recursive /custom_component/* config/custom_components/whatsapp/
 bashio::log.info "Installed custom component."
